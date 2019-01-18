a2dc65a9deb7292310d99629d8bb545f12737a51
willnx <willnx84@gmail.com>
2019-01-18 14:16:26 -0800

    Opened NTP in firewall

--- vm/configure.sh
+++ vm/configure.sh
@@ -133,6 +133,10 @@ setup_nat () {
   iptables -A OUTPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
   iptables -A INPUT -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 
+  # Enable NTP
+  iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
+  iptables -A INPUT -p udp --sport 123 -j ACCEPT
+
   # Enable ICMP (aka ping)
   iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
   iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
