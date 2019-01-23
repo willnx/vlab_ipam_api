097f9bd5919d345be87c80424b3f2e7420786ae7
willnx <willnx84@gmail.com>
2019-01-23 15:30:19 -0800

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-ipam-api",
       author="Nicholas Willhite,",
       author_email='willnx84@gmail.com',
-      version='2019.01.23',
+      version='2019.01.24',
       packages=find_packages(),
       include_package_data=True,
       classifiers=[
